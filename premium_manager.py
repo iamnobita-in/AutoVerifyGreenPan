66ec0cfb9b4b6a9dbdf360dcde75b62f73285460
iamnobita-in <iamnobita1076@gmail.com>
2026-06-30 15:03:21 +0530

    Create premium_manager.py

---
+++ premium_manager.py
@@ -0,0 +1,19 @@
+import json
+import os
+from datetime import datetime
+
+FILE_NAME = 'premium_users.json'
+
+def load_premium():
+    if not os.path.exists(FILE_NAME): return {}
+    with open(FILE_NAME, 'r') as f: return json.load(f)
+
+def save_premium(data):
+    with open(FILE_NAME, 'w') as f: json.dump(data, f, indent=4)
+
+def is_premium(chat_id):
+    data = load_premium()
+    expiry = data.get(str(chat_id))
+    if expiry and datetime.now().strftime('%Y-%m-%d') <= expiry:
+        return True
+    return False
