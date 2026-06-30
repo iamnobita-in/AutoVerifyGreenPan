629ae359eb79fb5da9fbf1440cc500cf5720fb4d
iamnobita-in <iamnobita1076@gmail.com>
2026-06-30 15:27:36 +0530

    Update premium_manager.py

--- premium_manager.py
+++ premium_manager.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 FILE_NAME = 'premium_users.json'
-ADMIN_ID = "8734310359"  # <--- Yahan apni Telegram ID daalo
+ADMIN_ID = "8734310359"
 
 def load_premium():
     if not os.path.exists(FILE_NAME): return {}
@@ -12,12 +12,17 @@ def load_premium():
 def save_premium(data):
     with open(FILE_NAME, 'w') as f: json.dump(data, f, indent=4)
 
+def is_admin(chat_id):
+    return str(chat_id) == ADMIN_ID
+
 def is_premium(chat_id):
+    # Agar user Admin hai, toh access mil jayega
+    if is_admin(chat_id):
+        return True
+    
+    # Agar Admin nahi hai, toh premium file check karega
     data = load_premium()
     expiry = data.get(str(chat_id))
     if expiry and datetime.now().strftime('%Y-%m-%d') <= expiry:
         return True
     return False
-
-def is_admin(chat_id):
-    return str(chat_id) == ADMIN_ID
